ec0e989b481e4080830a4f74ea585003cf95c92d
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-04-14 17:36:49 +0200

    feat(log): Improved log formatter to include stack trace in logs shown in console

--- src/main/kotlin/fr/paralya/bot/LogFormater.kt
+++ src/main/kotlin/fr/paralya/bot/LogFormater.kt
@@ -1,6 +1,7 @@
 package fr.paralya.bot
 
 import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.classic.spi.IThrowableProxy
 import ch.qos.logback.core.LayoutBase
 import ch.qos.logback.core.encoder.LayoutWrappingEncoder
 import java.text.SimpleDateFormat
@@ -9,7 +10,7 @@ import java.util.*
 class LogFormater : LayoutWrappingEncoder<ILoggingEvent>() {
 	init {
 		layout = object : LayoutBase<ILoggingEvent>() {
-			private val format = "[%d{dd/MM/yyyy HH:mm:ss}] %highlight(%-5level) - %msg\n"
+			private val format = "[%d{dd/MM/yyyy HH:mm:ss}] %highlight(%-5level) - %msg%ex\n"
 
 			override fun doLayout(event: ILoggingEvent): String {
 				return format
@@ -19,7 +20,7 @@ class LogFormater : LayoutWrappingEncoder<ILoggingEvent>() {
 					.replace(
 						"%d{dd/MM/yyyy HH:mm:ss}",
 						SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(Date(event.timeStamp))
-					)
+					).replace("%ex", event.throwableProxy?.let { formatException(it) } ?: "")
 			}
 
 			private fun highlightLevel(level: String): String {
@@ -31,6 +32,30 @@ class LogFormater : LayoutWrappingEncoder<ILoggingEvent>() {
 					"CRITICAL" -> "\u001B[41m$level\u001B[0m" // Red background
 					else -> level
 				}
+			}
+
+			private fun formatException(throwableProxy: IThrowableProxy): String {
+				val stackTrace = StringBuilder()
+				stackTrace.append("\n\t").append(throwableProxy.className)
+				if (throwableProxy.message != null) {
+					stackTrace.append(": ").append(throwableProxy.message)
+				}
+				for (element in throwableProxy.stackTraceElementProxyArray) {
+					stackTrace.append("\n\t\tat ").append(element)
+				}
+				var cause = throwableProxy.cause
+				while (cause != null) {
+					stackTrace.append("\nCaused by: ").append(cause.className)
+					if (cause.message != null) {
+						stackTrace.append(": ").append(cause.message)
+					}
+					for (element in cause.stackTraceElementProxyArray) {
+						stackTrace.append("\n\t\tat ").append(element)
+					}
+					cause = cause.cause
+				}
+
+				return stackTrace.toString()
 			}
 		}
 	}
