27be4b9737f93d75dc84400823483215d7e0aa67
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2025-08-27 21:21:38 +0200

    feat(common): Add an object for versioning and compatibility management (will be used in each plugins to ensure compatibility)

---
+++ common/src/main/kotlin/fr/paralya/bot/common/CommonModule.kt
@@ -0,0 +1,10 @@
+package fr.paralya.bot.common
+
+/**
+ * Common module metadata, used to ensure correct versioning and compatibility
+ * with `compileOnly` dependencies.
+ */
+object CommonModule {
+    const val API_VERSION = "1.0.0"
+    const val MIN_COMPATIBLE_VERSION = "1.0.0"
+}
