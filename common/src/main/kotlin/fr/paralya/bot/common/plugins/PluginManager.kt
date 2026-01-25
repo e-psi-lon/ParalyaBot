759ce53d796c7a49a4a87c17ee5c133469c259f7
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-01-25 16:32:38 +0100

    feat(plugins): Send the `PluginReadyEvent` after plugin start to notify setup listeners

--- common/src/main/kotlin/fr/paralya/bot/common/plugins/PluginManager.kt
+++ common/src/main/kotlin/fr/paralya/bot/common/plugins/PluginManager.kt
@@ -1,14 +1,27 @@
 package fr.paralya.bot.common.plugins
 
+import dev.kordex.core.ExtensibleBot
+import dev.kordex.core.koin.KordExKoinComponent
 import dev.kordex.core.plugins.PluginManager
+import kotlinx.coroutines.flow.toSet
+import kotlinx.coroutines.launch
+import org.koin.core.component.inject
 import org.pf4j.PluginState
 import java.nio.file.Path
 
-class PluginManager(roots: List<Path>, enabled: Boolean) : PluginManager(roots, enabled) {
+class PluginManager(roots: List<Path>, enabled: Boolean) : PluginManager(roots, enabled), KordExKoinComponent {
 
 
     override fun startPlugin(pluginId: String?): PluginState? {
-        return super.startPlugin(pluginId)
+        return super.startPlugin(pluginId).also {
+            val bot by inject<ExtensibleBot>()
+            bot.kordRef.launch {
+                bot.send(PluginReadyEvent(
+                    pluginId ?: error("Plugin id couldn't be found."),
+                    bot.kordRef.guilds.toSet()
+                ))
+            }
+        }
     }
 
     override fun stopPlugin(pluginId: String?): PluginState? {
