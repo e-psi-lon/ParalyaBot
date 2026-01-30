631ae44623257b543719bcbd62f9ddf97b4364cc
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-01-30 22:37:53 +0100

    refactor(flake): Reorganize the flake with better scoping and more functions to properly build plugins in a reproducible environment

--- flake.nix
+++ flake.nix
@@ -9,124 +9,168 @@
         let
             system = "x86_64-linux";
             pkgs = nixpkgs.legacyPackages.${system};
-            lastCommitAsTimestamp = builtins.readFile (pkgs.runCommand "created-timestamp" {} ''
-                printf '%s' "$(date -u -d @${toString self.lastModified} +'%Y-%m-%dT%H:%M:%SZ')" > $out
-            '');
 
-            gradlePropsContent = builtins.readFile ./gradle.properties;
-            versionMatch = builtins.match ".*paralyabot\\.version=([^\\n]+).*" gradlePropsContent;
-            globalVersion = builtins.elemAt versionMatch 0;
-            jarName = "paralya-bot-${globalVersion}.jar";
-            headlessJdk = pkgs.jdk21.override {
-                headless = true;
-                enableGtk = false;
-                enableJavaFX = false;
-            };
-            jre21 = pkgs.jre21_minimal.override {
-                jdk = headlessJdk;
-                jdkOnBuild = headlessJdk;
-                modules = [ "java.base" "java.xml" "java.naming" "java.logging" "jdk.crypto.ec" ];
-            };
-            build-bot = pkgs.writeShellScriptBin "build-bot" ''
-                IMAGE_PATH=$(nix build .#paralyabot-image --no-sandbox --print-out-paths)
-                if [ -n "$IMAGE_PATH" ]; then
-                    "$IMAGE_PATH" | podman load
-                fi
-            '';
-            run-bot = pkgs.writeShellScriptBin "run-bot" ''
-              CONTAINER_NAME="''${1:-ParalyaBot}"
 
-              echo "Starting container: $CONTAINER_NAME..."
-              podman run \
-                --name "$CONTAINER_NAME" \
-                --replace \
-                --detach \
-                --volume "$PWD/container:/app/external:Z" \
-                localhost/paralyabot:latest
-              echo "Container $CONTAINER_NAME started successfully."
-            '';
-
-            build-and-run-bot = pkgs.writeShellScriptBin "build-and-run-bot" ''
-                ${build-bot}/bin/build-bot && ${run-bot}/bin/run-bot ''${1:-ParalyaBot}
-            '';
-
-            mkGradleBuild = { task, version, output, name, extension ? "jar" }:
-                pkgs.stdenv.mkDerivation {
-                    pname = name;
-                    version = version;
-                    src = ./.;
-                    buildInputs = [ pkgs.jdk21 pkgs.cacert ];
-                    dontConfigure = true;
-
-                    buildPhase = ''
-                        export GRADLE_USER_HOME=$(mktemp -d)
-                        export JAVA_HOME=${pkgs.jdk21}
-                        export TZ=UTC
-                        export LANG=C.UTF-8
-                        export LC_ALL=C.UTF-8
-                        export SOURCE_DATE_EPOCH=${toString self.lastModified}
-                        export JAVA_TOOL_OPTIONS="-Djava.properties.date=${lastCommitAsTimestamp}"
-
-                        ./gradlew ${task} \
-                            --no-daemon \
-                            --no-configuration-cache \
-                            --info \
-                            --stacktrace \
-                    '';
-
-                    installPhase = ''
-                        mkdir -p $out
-                        cp ${output} $out/${name}.${extension}
-                    '';
-                };
+            lastCommitAsTimestamp = let
+                d = self.lastModifiedDate;
+                year = builtins.substring 0 4 d;
+                month = builtins.substring 4 2 d;
+                day = builtins.substring 6 2 d;
+                hour = builtins.substring 8 2 d;
+                minute = builtins.substring 10 2 d;
+                second = builtins.substring 12 2 d;
+            in "${year}-${month}-${day}T${hour}:${minute}:${second}Z";
         in {
-            packages.${system} = {
-                paralyabot-jar = mkGradleBuild {
-                    task = "shadowJar";
-                    version = globalVersion;
-                    output = "build/libs/${jarName}";
-                    name = "paralyabot";
-                };
+            packages.${system} = 
+                let 
+                    gradlePropsContent = builtins.readFile ./gradle.properties;
+                    extractVersion = propertyName: let
+                        versionMatch = builtins.match ".*${propertyName}=[ \\t]*([^\\n\\r]+).*" gradlePropsContent;
+                    in builtins.elemAt versionMatch 0;
+                    extractPluginVersion = pluginName: extractVersion "plugin\\.${pluginName}\\.version";
 
+                    mkGradleBuild = { task, version, output, name, extension ? "jar", extraArgs ? "" }:
+                        pkgs.stdenv.mkDerivation {
+                            pname = name;
+                            version = version;
+                            src = ./.;
+                            buildInputs = with pkgs; [ jdk21 cacert ];
+                            dontConfigure = true;
 
-                paralyabot-image = pkgs.dockerTools.streamLayeredImage {
-                    name = "paralyabot";
-                    tag = "latest";
-                    created = lastCommitAsTimestamp;
-                    
-                    contents = [
-                        jre21
-                        pkgs.cacert
-                    ];
-                    
-                    extraCommands = ''
-                        mkdir -p app/config app/plugins
-                        cp ${self.packages.${system}.paralyabot-jar}/paralyabot.jar app/paralyabot.jar
-                    '';
-                    
-                    config = {
-                        Entrypoint = [ "${jre21}/bin/java" "-jar" "/app/paralyabot.jar" ];
-                        WorkingDir = "/app";
-                        Env = [
-                            "PARALYA_BOT_CONFIG_FILE=/app/external/config.conf"
-                            "PARALYA_BOT_PLUGINS_DIR=/app/external/plugins"
-                        ];
-                        Volumes = {
-                            "/app/external" = {};
+                            buildPhase = ''
+                                export GRADLE_USER_HOME=$(mktemp -d)
+                                export JAVA_HOME=${pkgs.jdk21}
+                                export TZ=UTC
+                                export LANG=C.UTF-8
+                                export LC_ALL=C.UTF-8
+                                export SOURCE_DATE_EPOCH=${toString self.lastModified}
+                                export JAVA_TOOL_OPTIONS="-Djava.properties.date=${lastCommitAsTimestamp}"
+
+                                ./gradlew ${task} \
+                                    --no-daemon \
+                                    --no-configuration-cache \
+                                    --info \
+                                    --stacktrace \
+                                    ${extraArgs}
+                            '';
+
+                            installPhase = ''
+                                mkdir -p $out
+                                cp ${output} $out/${name}.${extension}
+                            '';
                         };
-                    };
-                    
-                    maxLayers = 25;
-                };
-            };
+                in {
+                    paralyabot-jar = 
+                        let
+                            version = extractVersion "paralyabot\\.version";
+                        in
+                        mkGradleBuild {
+                            task = "shadowJar";
+                            version = version;
+                            output = "build/libs/paralya-bot-${version}.jar";
+                            name = "paralyabot";
+                        };
 
-            devShells.${system}.default = pkgs.mkShell {
-                buildInputs = with pkgs; [
-                    jdk21
-                    build-bot
-                    run-bot
-                    build-and-run-bot
-                ];
-            };
+                    lg-plugin = 
+                        let
+                            version = extractPluginVersion "lg";
+                        in
+                        mkGradleBuild {
+                            task = "lg:distZip";
+                            version = version;
+                            output = "lg/build/distributions/lg-${version}.zip";
+                            name = "lg-plugin-${version}";
+                            extension = "zip";
+                        };
+
+                    sta-plugin = 
+                        let
+                            version = extractPluginVersion "sta";
+                        in
+                        mkGradleBuild {
+                            task = "sta:distZip";
+                            version = extractPluginVersion "sta";
+                            output = "sta/build/distributions/sta-${version}.zip";
+                            name = "sta-plugin-${version}";
+                            extension = "zip";
+                        };
+
+                    paralyabot-image = 
+                        let 
+                            headlessJdk = pkgs.jdk21.override {
+                                headless = true;
+                                enableGtk = false;
+                                enableJavaFX = false;
+                            };
+                            jre21 = pkgs.jre21_minimal.override {
+                                jdk = headlessJdk;
+                                jdkOnBuild = headlessJdk;
+                                modules = [ "java.base" "java.xml" "java.naming" "java.logging" "jdk.crypto.ec" ];
+                            };
+                        in
+                        pkgs.dockerTools.streamLayeredImage {
+                            name = "paralyabot";
+                            tag = "latest";
+                            created = lastCommitAsTimestamp;
+                            
+                            contents = [
+                                jre21
+                                pkgs.cacert
+                            ];
+                            
+                            extraCommands = ''
+                                mkdir -p app/config app/plugins
+                                cp ${self.packages.${system}.paralyabot-jar}/paralyabot.jar app/paralyabot.jar
+                            '';
+                            
+                            config = {
+                                Entrypoint = [ "${jre21}/bin/java" "-jar" "/app/paralyabot.jar" ];
+                                WorkingDir = "/app";
+                                Env = [
+                                    "PARALYA_BOT_CONFIG_FILE=/app/external/config.conf"
+                                    "PARALYA_BOT_PLUGINS_DIR=/app/external/plugins"
+                                ];
+                                Volumes = {
+                                    "/app/external" = {};
+                                };
+                            };
+                            
+                            maxLayers = 25;
+                        };
+                };
+
+            devShells.${system}.default =
+                let 
+                    build-bot = pkgs.writeShellScriptBin "build-bot" ''
+                        IMAGE_PATH=$(nix build .#paralyabot-image --no-sandbox --print-out-paths)
+                        if [ -n "$IMAGE_PATH" ]; then
+                            "$IMAGE_PATH" | podman load
+                        fi
+                    '';
+                    run-bot = pkgs.writeShellScriptBin "run-bot" ''
+                    CONTAINER_NAME="''${1:-ParalyaBot}"
+
+                    echo "Starting container: $CONTAINER_NAME..."
+                    podman run \
+                        --name "$CONTAINER_NAME" \
+                        --replace \
+                        --detach \
+                        --volume "$PWD/container:/app/external:Z" \
+                        localhost/paralyabot:latest
+                    echo "Container $CONTAINER_NAME started successfully."
+                    '';
+
+                    build-and-run-bot = pkgs.writeShellScriptBin "build-and-run-bot" ''
+                        ${build-bot}/bin/build-bot && ${run-bot}/bin/run-bot ''${1:-ParalyaBot}
+                    '';
+                in
+                pkgs.mkShell {
+                    buildInputs = with pkgs; [
+                        jdk21
+                        build-bot
+                        run-bot
+                        build-and-run-bot
+                    ];
+                };
         };
 }
