a6caa78ded510949a29abc19a9d0a787d6eb7a56
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-03-10 17:04:57 +0100

    build(gradle): Adjust Gradle memory and suppress unused scope warning

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,9 +14,11 @@ kotlin {
 dependencies {
 	implementation(plugin(libs.plugins.kotlin.jvm))
 	implementation(plugin(libs.plugins.kover))
+	// implementation(plugin(libs.plugins.detekt))
 }
 
 
 
+@Suppress("UnusedReceiverParameter") // Enforce dependency scope
 fun DependencyHandlerScope.plugin(plugin: Provider<PluginDependency>): Provider<String> =
 	plugin.map { "${it.pluginId}:${it.pluginId}.gradle.plugin:${it.version}" }
