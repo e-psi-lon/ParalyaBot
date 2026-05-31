0a3dc017db977ad1caa1b74eab73a60c0bb2390c
Lilian Maulny (e_ψ_lon) <theg41g@gmail.com>
2026-05-31 15:29:59 +0200

    refactor(common): Allow to specify a class directly to avoid class loaders issues

--- common/src/main/kotlin/fr/paralya/bot/common/CoreUtils.kt
+++ common/src/main/kotlin/fr/paralya/bot/common/CoreUtils.kt
@@ -10,25 +10,39 @@ import dev.kord.core.entity.User
 import dev.kord.rest.Image
 import dev.kordex.core.utils.any
 import dev.kordex.core.utils.hasRole
+import fr.paralya.bot.common.plugins.Plugin
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.withContext
 
+private object Resource
+/**
+ * Retrieves an image asset from the specified path.
+ *
+ * @receiver The plugin instance from which the asset is being retrieved.
+ * @param path The path to the asset file (in the `assets` resource directory).
+ * @return The image as an [Image] object.
+ * @throws IllegalArgumentException if the resource is not found at the specified path.
+ */
+suspend inline fun <reified T : Plugin> T.getAsset(path: String) = getAsset(path, pluginId, T::class.java)
+
 /**
  * Retrieves an image asset from the specified path.
  *
  * @param path The path to the asset file (in the `assets` resource directory).
- * @param game An optional game to include in the asset path.
+ * @param game The game ID associated with the asset (optional).
+ * @param clazz The class from which the resource is being loaded (optional).
  * @return The image as an [Image] object.
- * @throws IllegalArgumentException if the resource is not found at the specified path.
+ * @throws IllegalArgumentException if the resource is not found at the specified path
  */
-suspend fun getAsset(path: String, game: String? = null) =
-    Image.raw(getResource("assets/${if (game != null) "$game/" else ""}$path.webp"), Image.Format.WEBP)
+suspend fun getAsset(path: String, game: String? = null, clazz: Class<*> = Resource.javaClass) =
+    Image.raw(getResource("assets/${if (game != null) "$game/" else ""}$path.webp", clazz), Image.Format.WEBP)
 
-private object Resource
-suspend fun getResource(path: String): ByteArray {
-    val resource = Resource.javaClass.getResourceAsStream("/$path")
+
+suspend inline fun <reified T>getResourceFrom(path: String) = getResource(path, T::class.java)
+suspend fun getResource(path: String, clazz: Class<*> = Resource.javaClass) : ByteArray {
+    val resource = clazz.getResourceAsStream("/$path")
         ?: throw IllegalArgumentException("Resource at path /$path not found")
     return withContext(Dispatchers.IO) {
         resource.readAllBytes().also {
